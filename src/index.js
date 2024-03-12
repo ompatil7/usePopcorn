3b6e53be0422f0dcb0f3bacde966624c0f5857d5
Om Patil <ompatil4749@gmail.com>
2024-03-12 08:56:30 +0530

    gitbash commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <StarRating maxRating={10} /> */}
+    {/* <StarRating sadsadsad maxRating={10} /> */}
     <App />
   </React.StrictMode>
 );
